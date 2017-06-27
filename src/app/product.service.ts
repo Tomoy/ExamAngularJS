f564250bd19258a6d57157716918efa91ceba1fb
Tomás Ignacio Moyano <moyanotomasi@gmail.com>
2017-06-27 23:47:34 +0200

    pink & red paths resueltos

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -10,78 +10,63 @@ import { ProductFilter } from './product-filter';
 @Injectable()
 export class ProductService {
 
-  constructor(
-    private _http: Http,
-    @Inject(BackendUri) private _backendUri) { }
+    constructor(
+        private _http: Http,
+        @Inject(BackendUri) private _backendUri) { }
 
-  getProducts(filter: ProductFilter = undefined): Observable<Product[]> {
+    getProducts(filter: ProductFilter = undefined): Observable<Product[]> {
+        /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
+        | Yellow Path                                                      |
+        |~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
+        | Pide al servidor que te retorne los productos filtrados por      |
+        | estado.                                                          |
+        |                                                                  |
+        | En la documentación de 'JSON Server' tienes detallado cómo       |
+        | filtrar datos en tus peticiones, pero te ayudo igualmente. La    |
+        | querystring debe tener estos parámetros:                         |
+        |                                                                  |
+        |   - Búsqueda por estado:                                         |
+        |       state=x (siendo x el estado)                               |
+        |~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+        let filtersToAdd: string = '';
 
-    /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
-    | Pink Path                                                        |
-    |~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
-    | Pide al servidor que te retorne los productos ordenados de más   |
-    | reciente a menos, teniendo en cuenta su fecha de publicación.    |
-    |                                                                  |
-    | En la documentación de 'JSON Server' tienes detallado cómo hacer |
-    | la ordenación de los datos en tus peticiones, pero te ayudo      |
-    | igualmente. La querystring debe tener estos parámetros:          |
-    |                                                                  |
-    |   _sort=publishedDate&_order=DESC                                |
-    |~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+        if (filter) {
 
-    /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
-    | Red Path                                                         |
-    |~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
-    | Pide al servidor que te retorne los productos filtrados por      |
-    | texto y/ por categoría.                                          |
-    |                                                                  |
-    | En la documentación de 'JSON Server' tienes detallado cómo       |
-    | filtrar datos en tus peticiones, pero te ayudo igualmente. La    |
-    | querystring debe tener estos parámetros:                         |
-    |                                                                  |
-    |   - Búsqueda por texto:                                          |
-    |       q=x (siendo x el texto)                                    |
-    |   - Búsqueda por categoría:                                      |
-    |       category.id=x (siendo x el identificador de la categoría)  |
-    |~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+            if (filter.category) {
+                //Para que si el usuario selecciona la primer categoría '-', muestre todos los productos. Solo, si es una categoría de las existentes, 1,2 o 3, filtra.
+                if (filter.category !== '0') {
+                    filtersToAdd = "&category.id=" + filter.category;
+                }
+            }
 
-    /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
-    | Yellow Path                                                      |
-    |~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
-    | Pide al servidor que te retorne los productos filtrados por      |
-    | estado.                                                          |
-    |                                                                  |
-    | En la documentación de 'JSON Server' tienes detallado cómo       |
-    | filtrar datos en tus peticiones, pero te ayudo igualmente. La    |
-    | querystring debe tener estos parámetros:                         |
-    |                                                                  |
-    |   - Búsqueda por estado:                                         |
-    |       state=x (siendo x el estado)                               |
-    |~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+            if (filter.text) {
+                filtersToAdd += "&q=" + filter.text;
+            }
+        }
 
-    return this._http
-      .get(`${this._backendUri}/products`)
-      .map((data: Response): Product[] => Product.fromJsonToList(data.json()));
-  }
+        return this._http
+            .get(`${this._backendUri}/products?_sort=publishedDate&_order=DESC` + filtersToAdd)
+            .map((data: Response): Product[] => Product.fromJsonToList(data.json()));
+    }
 
-  getProduct(productId: number): Observable<Product> {
-    return this._http
-      .get(`${this._backendUri}/products/${productId}`)
-      .map((data: Response): Product => Product.fromJson(data.json()));
-  }
+    getProduct(productId: number): Observable<Product> {
+        return this._http
+            .get(`${this._backendUri}/products/${productId}`)
+            .map((data: Response): Product => Product.fromJson(data.json()));
+    }
 
-  buyProduct(productId: number): Observable<Product> {
-    const body: any = { 'state': 'sold' };
-    return this._http
-      .patch(`${this._backendUri}/products/${productId}`, body)
-      .map((data: Response): Product => Product.fromJson(data.json()));
-  }
+    buyProduct(productId: number): Observable<Product> {
+        const body: any = { 'state': 'sold' };
+        return this._http
+            .patch(`${this._backendUri}/products/${productId}`, body)
+            .map((data: Response): Product => Product.fromJson(data.json()));
+    }
 
-  setProductAvailable(productId: number): Observable<Product> {
-    const body: any = { 'state': 'selling' };
-    return this._http
-      .patch(`${this._backendUri}/products/${productId}`, body)
-      .map((data: Response): Product => Product.fromJson(data.json()));
-  }
+    setProductAvailable(productId: number): Observable<Product> {
+        const body: any = { 'state': 'selling' };
+        return this._http
+            .patch(`${this._backendUri}/products/${productId}`, body)
+            .map((data: Response): Product => Product.fromJson(data.json()));
+    }
 
 }
